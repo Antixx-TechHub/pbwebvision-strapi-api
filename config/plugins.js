3818757f43a213c35afdd9d405367ae24d62d795
AntixxTechHub <info@antixxtechhub.com>
2023-03-23 13:30:14 +0530

    plugons.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,19 +1,19 @@
-// module.exports = ({ env }) => ({
-//   // ...
-//   upload: {
-//     // Update your cloudinary credentials here
-//     config: {
-//       provider: "cloudinary",
-//       providerOptions: {
-//       cloud_name: "antixx-techhub",
-//       api_key: "867656517325614",
-//       api_secret: "2wk2P3OGcGdsUo-Q_4Y4voIVE-U",
-//       },
-//       actionOptions: {
-//         upload: {},
-//         delete: {},
-//       },
-//     },
-//   },
-//   // ...
-// });
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    // Update your cloudinary credentials here
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+      cloud_name: "antixx-techhub",
+      api_key: "867656517325614",
+      api_secret: "2wk2P3OGcGdsUo-Q_4Y4voIVE-U",
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+  // ...
+});
